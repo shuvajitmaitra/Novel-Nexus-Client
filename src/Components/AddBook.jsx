944ae67ae8e2f1710103cd4e3cae0e9dc222e4ce
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-11-06 03:13:33 +0600

    add book route create and all books load to the database

--- src/Components/AddBook.jsx
+++ src/Components/AddBook.jsx
@@ -1,61 +1,112 @@
+import { useForm } from "react-hook-form";
 import Container from "./Container";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 
 const AddBook = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit =(data)=>{
+const {
+    bookName,
+    bookImage,
+    bookQuantity,
+    authorName,
+    category,
+    sortDescription,
+    bookRating,
+    bookSummary}= data;
+
+
+    console.log(bookName, bookImage, bookQuantity, authorName, category, sortDescription, bookRating, bookSummary);
+    axios.post("http://localhost:5000/books", {
+        book_name:bookName,
+        image:bookImage,
+        book_quantity:bookQuantity,
+        author_name:authorName,
+        category:category,
+        book_rating:bookRating,
+        short_description: sortDescription,
+        book_summary: bookSummary,
+    })
+    .then(res=>{
+        console.log(res.data);
+        if(res.data.insertedId){
+           return toast.success("Book Added Successfully")
+        }
+    })
+    .then(error=>{
+        toast.error(error)
+    })
+}
+
+
+
     return (
-        <Container>
-           <h1>Add Books Here!</h1>
-           <p>description</p>
-         <form className="card-body">
+        <Container >
+           <h1 className="text-3xl md:text-6xl py-6 font-bold text-primary text-center">Add Books Here!</h1>
+         <form onSubmit={handleSubmit(onSubmit)} className="card-body lg:w-3/4 mx-auto">
 {/* ------------------------------------------------ */}
 {/* First row*/}
 {/* ------------------------------------------------ */}
-        <div>
-        <div className="form-control">
+        <div className="w-full flex flex-col lg:flex-row gap-6">
+        <div className="form-control flex-1">
           <label className="label">
-            <span className="label-text">Email</span>
+            <span className="label-text">Book Name</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="text" {...register("bookName")} name="bookName" placeholder="Book Name" className="input input-bordered" required />
         </div>
-        <div className="form-control">
+
+        <div className="form-control flex-1">
           <label className="label">
-            <span className="label-text">Email</span>
+            <span className="label-text">Book Image</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="text" {...register("bookImage")} name="bookImage" placeholder="Book Image" className="input input-bordered" required />
         </div>
         </div>
 
 {/* ------------------------------------------------ */}
 {/* Second row*/}
 {/* ------------------------------------------------ */}
-      <div>
-      <div className="form-control">
+      <div className="w-full flex flex-col lg:flex-row gap-6">
+      <div className="form-control flex-1 ">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Book Quantity (Number)</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="number" {...register("bookQuantity")} name="bookQuantity" placeholder="Book Quantity" className="input input-bordered" required />
         </div>
-      <div className="form-control">
+      <div className="form-control flex-1">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Author Name</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="text" {...register("authorName")} name="authorName"  placeholder="Author Name" className="input input-bordered" required />
         </div>
       </div>
 {/* ------------------------------------------------ */}
 {/* First row*/}
 {/* ------------------------------------------------ */}
-<div>
-      <div className="form-control">
+<div className="w-full flex flex-col lg:flex-row gap-6">
+      <div className="form-control flex-1">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Book Category</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+         <select 
+           type="text"
+           {...register("category")} name="category"
+           className="input input-bordered rounded"
+           required>
+            <option selected disabled  id="">Choose a Category</option>
+            <option value="History"  id="">History</option>
+            <option value="Drama" id="">Drama</option>
+            <option value="Novel" id="">Novel</option>
+            <option value="Thriller" id="">Thriller</option>
+         </select>
         </div>
-      <div className="form-control">
+      <div className="form-control flex-1">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Sort Description</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="text" {...register("sortDescription")} name="sortDescription" placeholder="Sort Description" className="input input-bordered" required />
         </div>
       </div>
 
@@ -63,45 +114,33 @@ const AddBook = () => {
 {/* First row*/}
 {/* ------------------------------------------------ */}
 
-<div>
-      <div className="form-control">
+
+   <div className="flex flex-col lg:flex-row gap-6">
+   <div className="form-control flex-1">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Book Rating (Number)</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="number" {...register("bookRating")} name="bookRating"  placeholder="Book Summary" className="input input-bordered" required />
         </div>
-      <div className="form-control">
+      <div className="form-control flex-1">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Book Summary</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="text" {...register("bookSummary")} name="bookSummary"  placeholder="Book Summary" className="input input-bordered" required />
         </div>
-      </div>
+   </div>
+
+
 
 {/* ------------------------------------------------ */}
-{/* First row*/}
+{/*Submit Button*/}
 {/* ------------------------------------------------ */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+     <div className="form-control flex-1 mt-6">
+          <button type="submit" className="btn btn-primary">Add Book</button>
         </div>
       </form>
         </Container>
     );
-};
+    };
 
 export default AddBook;
