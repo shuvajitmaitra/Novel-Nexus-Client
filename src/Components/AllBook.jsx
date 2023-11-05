5c243b2a3a0582042dee01d83f40183e3cc619bd
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-11-06 03:22:48 +0600

    get all book in all books route and show all book in website

--- src/Components/AllBook.jsx
+++ src/Components/AllBook.jsx
@@ -1,8 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AllBook = () => {
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+        axios
+          .get("http://localhost:5000/books")
+          .then((res) => {
+            console.log(res.data);
+            setBooks(res.data);
+          })
+          .then((error) => {
+            console.log(error);
+          });
+      }, []);
     return (
         <div>
             <h3>all book</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                {
+                    books?.map(book=>
+                        <div key={book._id}>
+                            <img src={book.image} alt="" />
+                            <h2>{book.book_name}</h2>
+                        </div>
+                    )
+                }
+            </div>
+
         </div>
     );
 };
